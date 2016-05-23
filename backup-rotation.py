0259505514ce2e41f96c1e382906cc38c31642a8
Benjamin Leuckefeld <code@deratrox.de>
2016-05-23 17:18:16 +0200

    cleaned __init__, enhanced log output

--- backup-rotation.py
+++ backup-rotation.py
@@ -33,7 +33,7 @@ class Log(object):
             self.log_file.close()
 
     def log(self, text, log_level=1):
-        msg = "[{0}] [{1}] {2}".format(self.gettimestamp(), self.log_levels[log_level], text)
+        msg = "[{0}] [{1}] {2}".format(self.get_timestamp(), self.log_levels[log_level], text)
 
         if log_level >= self.log_level_file:
             self.log_file.write(msg + "\n")
@@ -42,14 +42,177 @@ class Log(object):
             print(msg)
 
     @staticmethod
-    def gettimestamp():
+    def get_timestamp():
         return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 class BackupRotation(object):
-    __version__ = "v1.1.0"
+    __version__ = "v1.1.1"
+
+    def __init__(self):
+        config_path = os.path.normpath(os.path.join(os.path.dirname(__file__), "config.json"))
+        log_path = ""
+        log_level_file = 1
+        log_level_console = 1
+
+        for i in range(1, len(sys.argv)):
+            arg = sys.argv[i]
+            if arg == "-q":
+                log_level_console = 4
+            elif arg == "-c":
+                if len(sys.argv) > i + 1:
+                    config_path = sys.argv[i + 1]
+            elif arg == "-l":
+                if len(sys.argv) > i + 1:
+                    log_path = sys.argv[i + 1]
+            elif arg == "-lv":
+                if len(sys.argv) > i + 1:
+                    log_level_file = int(sys.argv[i + 1])
+
+        self.log = Log(log_path, log_level_file, log_level_console).log
+
+        self.log("backup-rotation.py " + self.__version__)
+
+        if not os.path.isfile(config_path):
+            self.log("Stated configfile not found, aborting...", 3)
+            exit()
+
+        config_data = self.load_config(config_path)
+
+        self.now = datetime.now()
+
+        # Start rotation for every backup item
+        for backup_item in config_data["backup_items"]:
+            self.run_backup_rotation(backup_item)
+
+        self.log("Backup rotation finished.")
+
+    def run_backup_rotation(self, backup_item):
+        self.log("--- RUN: backup rotation for %s" % backup_item["source"])
+
+        if not backup_item["compression"] in ["gz", "xz", "bz2"]:
+            self.log("Unknown compression type \"{0}\". Using lzma.".format(backup_item["compression"]), 2)
+            backup_item["compression"] = "xz"
+
+        file_extension = ".tar." + backup_item["compression"]
+
+        file_prefix = self.now.strftime("%Y-%m-%d")
+
+        no_backups_created = True
+
+        timetuple = self.now.timetuple()
+
+        for max_backups, current_date, backup_date, period_name in [
+            (backup_item["daily_backups"], None, None, "DAILY"),
+            (backup_item["weekly_backups"], timetuple.tm_wday, backup_item["create_backup_day_of_week"], "WEEKLY"),
+            (backup_item["monthly_backups"], timetuple.tm_mday, backup_item["create_backup_day_of_month"],
+             "MONTHLY"),
+            (backup_item["yearly_backups"], timetuple.tm_yday, backup_item["create_backup_day_of_year"], "YEARLY")
+        ]:
+            if max_backups > 0:
+                if current_date == backup_date:
+                    file_name = file_prefix + "-" + period_name + file_extension
+                    self.create_backup(backup_item, file_name)
+                    no_backups_created = False
+
+        if no_backups_created:
+            self.log("No backups created.")
+
+        # Check for old backups
+        backups = os.listdir(backup_item["destination"])
+
+        old_backups = defaultdict(list)
+
+        for str_ in backups:
+            match = re.match("\d{4}-\d{2}-\d{2}-(DAILY|WEEKLY|MONTHLY|YEARLY)" + re.escape(file_extension), str_)
+
+            if match:
+                old_backups[match.group(1)].append(os.path.normpath(os.path.join(backup_item["destination"], str_)))
+
+        # Check for overhang in old backups and delete it
+        for period_name, max_backups in [
+            ('DAILY', backup_item["daily_backups"]),
+            ('WEEKLY', backup_item["weekly_backups"]),
+            ('MONTHLY', backup_item["monthly_backups"]),
+            ('YEARLY', backup_item["yearly_backups"]),
+        ]:
+
+            files = old_backups[period_name]
+            overhang = len(files) - max_backups
+
+            if overhang > 0:
+                self.log(
+                    "Overhang found ({0} backups). Deleting {1} old backup(s)...".format(period_name, overhang))
+
+                files = sorted(files, key=os.path.getctime)
+
+                for i in range(0, overhang):
+                    self.log("Deleting {0}...".format(os.path.basename(files[i])))
+                    os.remove(files[i])
+
+        self.log("--- END: backup rotation for %s" % backup_item["source"])
+
+    def create_backup(self, backup_item, file_name):
+        self.log("Creating backup... Filename: %s" % file_name)
+
+        mode = "w:" + backup_item["compression"]
+
+        file_path = os.path.normpath(os.path.join(backup_item["destination"], file_name))
+
+        if os.path.exists(file_path):
+            self.log("%s already exists. Skipping..." % file_name)
+            return
+
+        with tarfile.open(file_path, mode) as tar:
+            BackupRotation.add(self, tar, backup_item["source"], arcname=os.path.basename(backup_item["source"]))
+
+    def load_config(self, config_path: str) -> dict:
+        with open(config_path) as config_file:
+            data = json.load(config_file)
+
+        result = dict()
+
+        result["default"] = {
+            'create_backup_day_of_week': 0,
+            'create_backup_day_of_month': 1,
+            'create_backup_day_of_year': 1,
+
+            'daily_backups': 7,
+            'weekly_backups': 4,
+            'monthly_backups': 6,
+            'yearly_backups': 4,
+
+            'compression': 'xz',
+        }
+
+        result["default"].update(data["default"])
+
+        backup_items = list()
+
+        i = 0
+        for raw_backup_item in data["backup_items"]:
+            if "source" not in raw_backup_item or "destination" not in raw_backup_item:
+                self.log("Backup item no {0} invalid. Skipping...".format(i), 2)
+            elif not os.path.isdir(raw_backup_item["source"]):
+                self.log("Source path \"{0}\" not valid! (backup item no {1})".format(raw_backup_item["source"], i), 2)
+            elif not os.path.isdir(raw_backup_item["destination"]):
+                self.log(
+                    "Destination path \"{0}\" not valid! (backup item no {1})".format(raw_backup_item["source"], i), 2)
+            else:
+                backup_item = result["default"].copy()
+                backup_item.update(raw_backup_item)
+                backup_items.append(backup_item)
+
+            i += 1
+
+        result["backup_items"] = backup_items
+
+        self.log("Configuration loaded: %s" % config_path, 1)
+
+        return result
 
     # modified copy of tarfile.add (https://hg.python.org/cpython/file/v3.5.1/Lib/tarfile.py)
+    # noinspection PyProtectedMember,SpellCheckingInspection,PyShadowingBuiltins
     def add(self, tar, name, arcname=None, recursive=True, exclude=None, *, filter=None):
         tar._check("aw")
 
@@ -94,7 +257,7 @@ class BackupRotation(object):
                 try:
                     tar.addfile(tarinfo, f)
                 except Exception as e:
-                    self.log("An error occurred: %s" % e.strerror, 3)
+                    self.log("An error occurred: %s" % e, 3)
 
         elif tarinfo.isdir():
             tar.addfile(tarinfo)
@@ -104,167 +267,13 @@ class BackupRotation(object):
                         tar.add(os.path.join(name, f), os.path.join(arcname, f),
                                 recursive, exclude, filter=filter)
                     except Exception as e:
-                        self.log("An error occurred: %s" % e.strerror, 3)
+                        self.log("An error occurred: %s" % e, 3)
 
         else:
             try:
                 tar.addfile(tarinfo)
             except Exception as e:
-                self.log("An error occurred: %s" % e.strerror, 3)
-
-    def create_backup(self, backup_item, file_name):
-        self.log("Creating backup... Filename: %s" % file_name)
-
-        mode = "w:" + backup_item["compression"]
-
-        file_path = os.path.normpath(os.path.join(backup_item["destination"], file_name))
-
-        if os.path.exists(file_path):
-            self.log("%s already exists. Skipping..." % file_name)
-            return
-
-        with tarfile.open(file_path, mode) as tar:
-            BackupRotation.add(self, tar, backup_item["source"], arcname=os.path.basename(backup_item["source"]))
-
-    def load_config(self, config_path):
-        with open(config_path) as config_file:
-            data = json.load(config_file)
-
-        result = dict()
-
-        result["default"] = {
-            'create_backup_day_of_week': 0,
-            'create_backup_day_of_month': 1,
-            'create_backup_day_of_year': 1,
-
-            'daily_backups': 7,
-            'weekly_backups': 4,
-            'monthly_backups': 6,
-            'yearly_backups': 4,
-
-            'compression': 'xz',
-        }
-
-        result["default"].update(data["default"])
-
-        backup_items = list()
-
-        i = 0
-        for raw_backup_item in data["backup_items"]:
-            if "source" not in raw_backup_item or "destination" not in raw_backup_item:
-                self.log("Backup item no {0} invalid. Skipping...".format(i), 2)
-            elif not os.path.isdir(raw_backup_item["source"]):
-                self.log("Source path \"{0}\" not valid! (backup item no {1})".format(raw_backup_item["source"], i), 2)
-            elif not os.path.isdir(raw_backup_item["destination"]):
-                self.log("Destination path \"{0}\" not valid! (backup item no {1})".format(raw_backup_item["source"], i), 2)
-            else:
-                backup_item = result["default"].copy()
-                backup_item.update(raw_backup_item)
-                backup_items.append(backup_item)
-
-            i += 1
-
-        result["backup_items"] = backup_items
-
-        return result
-
-    def __init__(self):
-        config_path = os.path.normpath(os.path.join(os.path.dirname(__file__), "config.json"))
-        log_path = ""
-        log_level_file = 1
-        log_level_console = 1
-
-        for i in range(1, len(sys.argv)):
-            arg = sys.argv[i]
-            if arg == "-q":
-                log_level_console = 4
-            elif arg == "-c":
-                if len(sys.argv) > i + 1:
-                    config_path = sys.argv[i + 1]
-            elif arg == "-l":
-                if len(sys.argv) > i + 1:
-                    log_path = sys.argv[i + 1]
-            elif arg == "-lv":
-                if len(sys.argv) > i + 1:
-                    log_level_file = int(sys.argv[i + 1])
-
-        self.log = Log(log_path, log_level_file, log_level_console).log
-
-        self.log("backup-rotation.py " + self.__version__)
-
-        if not os.path.isfile(config_path):
-            self.log("Stated configfile not found, aborting...", 3)
-            exit()
-
-        config_data = self.load_config(config_path)
-        self.log("Configuration loaded: %s" % config_path, 1)
-
-        now = datetime.now()
-
-        # Start rotation for every backup item
-        for backup_item in config_data["backup_items"]:
-            self.log("Starting backup routine for %s..." % backup_item["source"])
-
-            if not backup_item["compression"] in ["gz", "xz", "bz2"]:
-                self.log("Unknown compression type \"{0}\". Using lzma.".format(backup_item["compression"]), 2)
-                backup_item["compression"] = "xz"
-
-            file_extension = ".tar." + backup_item["compression"]
-
-            file_prefix = now.strftime("%Y-%m-%d")
-
-            no_backups_created = True
-
-            timetuple = now.timetuple()
-
-            for max_backups, current_date, backup_date, period_name in [
-                (backup_item["daily_backups"], None, None, "DAILY"),
-                (backup_item["weekly_backups"], timetuple.tm_wday, backup_item["create_backup_day_of_week"], "WEEKLY"),
-                (backup_item["monthly_backups"], timetuple.tm_mday, backup_item["create_backup_day_of_month"],
-                 "MONTHLY"),
-                (backup_item["yearly_backups"], timetuple.tm_yday, backup_item["create_backup_day_of_year"], "YEARLY")
-            ]:
-                if max_backups > 0:
-                    if current_date == backup_date:
-                        file_name = file_prefix + "-" + period_name + file_extension
-                        self.create_backup(backup_item, file_name)
-                        no_backups_created = False
-
-            if no_backups_created:
-                self.log("No backups created.")
-
-            # Check for old backups
-            backups = os.listdir(backup_item["destination"])
-
-            old_backups = defaultdict(list)
-
-            for str_ in backups:
-                match = re.match("\d{4}-\d{2}-\d{2}-(DAILY|WEEKLY|MONTHLY|YEARLY)" + re.escape(file_extension), str_)
-
-                if match:
-                    old_backups[match.group(1)].append(os.path.normpath(os.path.join(backup_item["destination"], str_)))
-
-            # Check for overhang in old backups and delete it
-            for period_name, max_backups in [
-                ('DAILY', backup_item["daily_backups"]),
-                ('WEEKLY', backup_item["weekly_backups"]),
-                ('MONTHLY', backup_item["monthly_backups"]),
-                ('YEARLY', backup_item["yearly_backups"]),
-            ]:
-
-                files = old_backups[period_name]
-                overhang = len(files) - max_backups
-
-                if overhang > 0:
-                    self.log("Overhang found ({0} backups). Deleting {1} old backup(s)...".format(period_name, overhang))
-
-                    files = sorted(files, key=os.path.getctime)
-
-                    for i in range(0, overhang):
-                        self.log("Deleting {0}...".format(os.path.basename(files[i])))
-                        os.remove(files[i])
-
-        self.log("Backup rotation finished.")
+                self.log("An error occurred: %s" % e, 3)
 
 
 if __name__ == "__main__":
